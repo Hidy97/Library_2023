07f1ab91bdd173eeeb53beca384cdf59ea6d19c7
Hidy97 <csaladi.alexandra@gmail.com>
2023-09-12 14:00:42 +0200

    adatbázis szerkezete készen van

---
+++ library/app/Models/Copy.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Copy extends Model
+{
+    use HasFactory;
+    protected $primarykey = 'copy_id';
+}
